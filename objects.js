a0738051a02ccedfae8ee9db115c5b2bb3f0dde7
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 14:27:49 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,1 +1,6 @@
-var playlist = {Green Day:'Basket Case'};
+var playlist = [item:{Green Day:'Basket Case'}];
+
+function updatePlaylist(playlist,artist,title){
+    playlist.push({Foo Fighters:"Monkey Wrench"});
+    return playlist;
+}
