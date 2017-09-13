8ed591264297eae02b38391ea3b7e0c22b02bf7c
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 15:38:00 +0000

    automatically backed up by learn

--- test/objects-test.js
+++ test/objects-test.js
@@ -14,14 +14,14 @@ describe('objects', () => {
     expect(typeof playlist).to.equal('object')
     expect(Object.keys(playlist).length).to.be.greaterThan(0)
   })
-/*
+
   describe('updatePlaylist(playlist, artistName, songTitle)', () => {
     it('adds the `artistName: songTitle` key-value pair to `playlist`', () => {
       expect(updatePlaylist({}, 'Phil Ochs', "Here's to the State of Mississippi")).
         to.eql({ 'Phil Ochs': "Here's to the State of Mississippi" })
     })
   })
-
+/*
   describe('removeFromPlaylist(playlist, artistName)', () => {
     it('removes `artistName` from `playlist`', () => {
       expect(removeFromPlaylist({ Kanye: "Gold Digger" }, "Kanye")).
