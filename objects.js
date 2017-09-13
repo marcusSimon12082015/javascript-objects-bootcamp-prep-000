53796586c0d2e1bc5ae014ec9b1a1c84c102f011
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-09-13 14:29:28 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(playlist,artist,title){
     playlist.push({Foo Fighters:"Monkey Wrench"});
     return playlist;
 }
+
+function removeFromPlaylist(playlist,artist){
+  delete playlist.artist;
+  return playlist;
+}
